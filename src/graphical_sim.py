be99710bf52567ea2b1c10b551a84727d1885e22
tusharhero <tusharhero@sdf.org>
2023-06-01 23:48:16 +0530

    add few mroe states

--- src/graphical_sim.py
+++ src/graphical_sim.py
@@ -17,6 +17,7 @@
 
 import pygame as pg
 import pygame_gui as pgui
+import constants
 
 
 class State:
@@ -24,10 +25,10 @@ class State:
         self, surf: pg.Surface, manager_size: tuple[int, int]
     ) -> None:
         self.manager = pgui.UIManager(manager_size)
-        self.screen = surf
+        self.surface = surf
 
     def render(self) -> None:
-        self.manager.draw_ui(self.screen)
+        self.manager.draw_ui(self.surface)
 
     def update(self, events: list[pg.Event], time_delta: float):
         for event in events:
@@ -36,72 +37,92 @@ class State:
         return None
 
 
+class MainScreen(State):
+    def __init__(self, surf: pg.Surface) -> None:
+        size = height, width = surf.get_size()
+        super().__init__(surf, surf.get_size())
+        gui_rect = pg.Rect(-height, -width // 2, height, width // 2)
+        self.game_view = pg.Surface((500, 500))
+        self.container = pgui.core.UIContainer(
+            gui_rect,
+            self.manager,
+            anchors={"right": "right", "bottom": "bottom"},
+        )
+        self.button = pgui.elements.UIButton(
+            pg.Rect(0, 0, -1, -1),
+            "start",
+            self.manager,
+            self.container,
+            anchors={"center": "center"},
+        )
+
+    def render(self) -> None:
+        self.manager.draw_ui(self.surface)
+
+
 class TitleScreen(State):
     def __init__(self, surf: pg.Surface, text: str) -> None:
-        font = pg.font.SysFont("monospace", 25)
-        self.title_surf = font.render(text, True, "white")
-        self.screen = surf
+        self.font = pg.font.SysFont("monospace", 25)
+        self.title_surf = self.font.render(text, True, "white")
+        self.surface = surf
 
     def render(self) -> None:
         self.title_surf = pg.transform.scale(
             self.title_surf,
-            (self.screen.get_width(), self.title_surf.get_height()),
+            (self.surface.get_width(), self.title_surf.get_height()),
         )
         self.rect = self.title_surf.get_rect(
-            center=self.screen.get_rect().center
+            center=self.surface.get_rect().center
         )
-        self.screen.blit(self.title_surf, self.rect)
-
-    def fade(self):
-        # current_alpha = self.title_surf.get_alpha()
-        self.title_surf.set_alpha(0)
+        self.surface.blit(self.title_surf, self.rect)
 
     def update(self, events: list[pg.Event], time_delta: float):
         for event in events:
             if event.type == pg.KEYDOWN:
-                self.fade()
                 return 1
-        rect = self.title_surf.get_rect(center=self.screen.get_rect().center)
+        self.rect = self.title_surf.get_rect(
+            center=self.surface.get_rect().center
+        )
         return None
 
-    def next_state():
-        self.state = states[self.state_index + 1]
 
+class TextScreen(State):
+    def __init__(self, surf: pg.Surface, text: str) -> None:
+        super().__init__(surf, surf.get_size())
+        self.text_box = pgui.elements.UITextBox(
+            text, self.surface.get_rect(), self.manager
+        )
 
-class GameScreen(State):
-    pass
+    def update(self, events: list[pg.Event], time_delta: float):
+        for event in events:
+            if event.type == pg.KEYDOWN:
+                return 2
+        super().update(events, time_delta)
 
 
 class StateMachine:
     def __init__(self, states: list[State]):
         self.states: list[State] = states
-        self.state: int = 0
+        self.state_index: int = 0
 
     def run_state(self, events: list[pg.Event], time_delta: float):
-        state = self.states[self.state]
+        state = self.states[self.state_index]
         new_state = state.update(events, time_delta)
         state.render()
-        self.state = new_state if new_state or new_state == 0 else self.state
+        self.state_index = (
+            new_state if new_state or new_state == 0 else self.state_index
+        )
 
 
 def main():
-    title_ascii_art = """
- ______   _______  _______          _________ _       _________ _______ 
-(  __  \ (  ___  )(  ____ )|\     /|\__   __/( (    /|\__   __/(  ___  )
-| (  \  )| (   ) || (    )|| )   ( |   ) (   |  \  ( |   ) (   | (   ) |
-| |   ) || (___) || (____)|| | _ | |   | |   |   \ | |   | |   | |   | |
-| |   | ||  ___  ||     __)| |( )| |   | |   | (\ \) |   | |   | |   | |
-| |   ) || (   ) || (\ (   | || || |   | |   | | \   |   | |   | |   | |
-| (__/  )| )   ( || ) \ \__| () () |___) (___| )  \  |___) (___| (___) |
-(______/ |/     \||/   \__/(_______)\_______/|/    )_)\_______/(_______)
-    """
     pg.init()
-    screen = pg.display.set_mode((600, 500), pg.SCALED | pg.RESIZABLE)
+    screen = pg.display.set_mode((1000, 750), pg.SCALED | pg.RESIZABLE)
     pg.display.set_caption("darwinio")
     clock = pg.time.Clock()
-    title = TitleScreen(screen, title_ascii_art)
-    game = TitleScreen(screen, "Game")
-    statemachine = StateMachine([title, game])
+    title = TitleScreen(screen, constants.title_ascii_art)
+    license_notice = TextScreen(screen, constants.license_notice)
+    main_game = MainScreen(screen)
+    statemachine = StateMachine([title, license_notice, main_game])
 
     while True:
         time_delta = clock.tick(60) / 1000.0
@@ -110,6 +131,9 @@ def main():
             if event.type == pg.QUIT:
                 pg.quit()
                 raise SystemExit
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_F11:
+                    pg.display.toggle_fullscreen()
         screen.fill("black")
         statemachine.run_state(events, time_delta)
         pg.display.flip()
