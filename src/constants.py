ae67e144f2878e12d772233432f912f7e5ef0e8e
tusharhero <tusharhero@sdf.org>
2023-06-02 20:45:03 +0530

    add missing constant.py

---
+++ src/constants.py
@@ -0,0 +1,49 @@
+# The GPLv3 License (GPLv3)
+
+# Copyright © 2023 Tushar Maharana, and Mihir Nallagonda
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+title_ascii_art = """
+ ______   _______  _______          _________ _       _________ _______ 
+(  __  \ (  ___  )(  ____ )|\     /|\__   __/( (    /|\__   __/(  ___  )
+| (  \  )| (   ) || (    )|| )   ( |   ) (   |  \  ( |   ) (   | (   ) |
+| |   ) || (___) || (____)|| | _ | |   | |   |   \ | |   | |   | |   | |
+| |   | ||  ___  ||     __)| |( )| |   | |   | (\ \) |   | |   | |   | |
+| |   ) || (   ) || (\ (   | || || |   | |   | | \   |   | |   | |   | |
+| (__/  )| )   ( || ) \ \__| () () |___) (___| )  \  |___) (___| (___) |
+(______/ |/     \||/   \__/(_______)\_______/|/    )_)\_______/(_______)
+    """
+
+license_notice = """
+The GPLv3 License (GPLv3)
+
+Copyright © 2023 Tushar Maharana, and Mihir Nallagonda
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+with open("../LICENSE.md", "r") as file:
+    license = file.read()
