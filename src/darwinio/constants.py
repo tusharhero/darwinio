cb28d614e1694e6c8e43bc627ef7b6e588f189d3
tusharhero <tusharhero@sdf.org>
2023-06-10 10:00:18 +0530

    fix error in constants.py

--- src/darwinio/constants.py
+++ src/darwinio/constants.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-TITLE_ACSII_ART = """
+TITLE_ASCII_ART = """
  ______   _______  _______          _________ _       _________ _______ 
 (  __  \ (  ___  )(  ____ )|\     /|\__   __/( (    /|\__   __/(  ___  )
 | (  \  )| (   ) || (    )|| )   ( |   ) (   |  \  ( |   ) (   | (   ) |
